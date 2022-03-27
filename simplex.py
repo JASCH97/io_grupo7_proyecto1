a8242b9b0f4a0e8c04e77703613e95d75bd7635d
Giovanna Barra <66135504+indonirth@users.noreply.github.com>
2022-03-27 17:30:11 -0600

    Se modifica para que incluya la gran M

--- simplex.py
+++ simplex.py
@@ -1,7 +1,7 @@
 from readAndSaveTxtInformation import *
 from validationsBeforeMethodSelection import *
 from simplexMethod import *
-#from greatM import *
+from greatM import *
 
 validateInputFile()
 extractInformationFromRestrictions()     # se lee el documento y se extraen en listas toda la informacion necesaria
@@ -14,8 +14,8 @@ if selectedMethod[0] == 0 and verifyInequalities() == "simplex":                
     simplexMethod()
 
 elif selectedMethod[0] == 1 and verifyInequalities() == "no simplex":
-    augmentedForm() # para gran M
-    defineStarterBasicAndNoBasicVariables()
-    #createGreatMTabularForm()
-    simplexMethod()
+    augmentedFormGreatM() # para gran M
+    defineStarterBasicAndNoBasicVariables(1)
+    createGreatMTabularForm()
+    #simplexMethod()
 #NOTA IMPORTANTE: Declarar varaibles e igualarlas a alguna otra no funciona de manera global, con append si!!
