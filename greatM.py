420129d6f7296b9f73c7fcfb786bb951b48523e2
Giovanna Barra <66135504+indonirth@users.noreply.github.com>
2022-03-30 13:01:17 -0600

    Se eliminan y cambian nombres de las funciones que comparten Gran M y Dos Fases

--- greatM.py
+++ greatM.py
@@ -4,12 +4,12 @@ from readAndSaveTxtInformation import *
 import numpy as np
 
 """
-Function: appropriateForm
+Function: appropriateFormGreatM
 Input: -
 Output: -
 Description: Function that turns the appropriate form of the row (0) / objective function
 """
-def appropriateForm():
+def appropriateFormGreatM():
     objectiveFunction = restrictionsMatrix[0]
     artCounter = 0
     i = numberOfDecisionVariables[0]
@@ -22,33 +22,20 @@ def appropriateForm():
             artCounter += 1
             i += 1
         elif int(var[1]) > i:
-            objectiveFunction.insert(i, 0.0)
+            objectiveFunction.insert(i, 0)
             i += 1
     restrictionsMatrix[0] = checkMinimization(objectiveFunction)
 
-    appropriateFormOperations()
-    removeRightSide()
-
-"""
-Function: checkMinimization
-Input: objective function
-Output: transformed objective function
-Description: Function that checks if the problem has to be multiplied by -1
-"""
-def checkMinimization(objectiveFunction):
-    if optimization[0] == 'min':
-        return [i * -1 for i in objectiveFunction]
-    else:
-        return objectiveFunction
-
+    appropriateFormOperationsGreatM()
+    removeRightSideMethod()
     
 """
-Function: appropriateFormOperations
+Function: appropriateFormOperationsGreatM
 Input: -
 Output: -
 Description: Function that performs operations on the rows with artificial variables
 """
-def appropriateFormOperations():
+def appropriateFormOperationsGreatM():
     operationsMatrix = []
     positions = []
 
@@ -70,52 +57,3 @@ def appropriateFormOperations():
     operationsMatrix.insert(0, restrictionsMatrix[0])   # objective function will always be in operations matrix
     result = np.sum(operationsMatrix, axis=0)
     restrictionsMatrix[0] = result.tolist()
-
-"""
-Function: removeRightSide
-Input: -
-Output: -
-Description: Function that takes the restrictions and separates the right side from the slack variables
-"""
-def removeRightSide():
-    global rightSide
-    global restrictionsMatrix
-
-    # removes right side values from restrictions matrix
-    for restriction in restrictionsMatrix:
-        rightSide.append(restriction[len(restriction) - 1])
-        restriction.remove(restriction[len(restriction) - 1])
-
-"""
-Function: createGreatMTabularForm
-Input: -
-Output: -
-Description: Function that takes the restrictions and adds 0s and 1s based on the slack variables
-"""
-def createGreatMTabularForm():
-    i = 1
-    nextOnePosition = len(restrictionsMatrix[1]) - 1
-    while i <= len(restrictionsMatrix) - 1:
-        contVariablesLeft = contVariables[0] - len(restrictionsMatrix[i]) + 1
-        
-        while contVariablesLeft != 0:
-            restrictionsMatrix[i].insert(nextOnePosition,0)
-            contVariablesLeft -= 1
-        i += 1
-    
-    i = 1
-    j = 0
-    while i <= len(restrictionsMatrix) - 1:
-        if slackVariables[j][0] == 'x':                                     # checks if its normal variable
-            restrictionsMatrix[i][int(slackVariables[j][1]) - 1] = 1
-            j += 1
-            i += 1
-        if slackVariables[j][0] == 's':                                     # checks if its excess variable
-            restrictionsMatrix[i][int(slackVariables[j][1]) - 1] = -1
-            j += 1
-        if slackVariables[j][0] == 'a':                                     # checks if its artificial variable
-            restrictionsMatrix[i][int(slackVariables[j][1]) - 1] = 1
-            j += 1
-        i += 1
-    
-    appropriateForm()
